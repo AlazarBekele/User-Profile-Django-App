26853fa48909fd32195ce47d771c1069f8fbb3f9
AlazarBekele <alazarthe70@gmail.com>
2025-11-22 17:26:24 +0300

    Create User Profile model

--- UserProfile/admin.py
+++ UserProfile/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 
 # Register your models here.
 admin.site.unregister(Group)
+
+class UserAdmin (admin.ModelAdmin):
+
+    model = User
+    fields = ['username', 'email', 'first_name', 'last_name']
+
+# unregister the User
+admin.site.unregister(User)
+
+# register the User again
+admin.site.register (User, UserAdmin)
