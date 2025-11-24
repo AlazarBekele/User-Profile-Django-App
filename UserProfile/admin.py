ff6ac2ca839de95f3c269022d9db58bf034550a9
AlazarBekele <alazarthe70@gmail.com>
2025-11-24 20:01:40 +0300

    Create Follow code

--- UserProfile/admin.py
+++ UserProfile/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
+from .models import Profile
 
 # Register your models here.
 admin.site.unregister(Group)
+
+class ProfileAddUser (admin.StackedInline):
+    model = Profile
 
 class UserAdmin (admin.ModelAdmin):
 
     model = User
     fields = ['username', 'email', 'first_name', 'last_name']
+    inlines = [ProfileAddUser]
 
 # unregister the User
 admin.site.unregister(User)
--- UserProfile/models.py
+++ UserProfile/models.py
@@ -7,3 +7,6 @@ class Profile (models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     follows = models.ManyToManyField("self", related_name="followed_by", symmetrical=False, blank=True)
+
+    def __str__(self):
+        return self.user.username
